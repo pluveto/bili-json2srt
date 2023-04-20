b0e8e840410bd19feaab882e1e4af75cba54a067
Zijing Zhang <50045289+pluveto@users.noreply.github.com>
2023-04-21 00:36:05 +0800

    chore: rename main.py to cvt.py

--- cvt.py
+++ cvt.py
@@ -4,13 +4,13 @@ import sys
 def convert_to_srt(data):
     srt_output = ""
     for index, item in enumerate(data["body"]):
-        start_time = int(item["from"])
-        end_time = int(item["to"])
+        start_time = int(item["from"] * 1000)
+        end_time = int(item["to"] * 1000)
         content = item["content"]
 
         # Format timestamps
-        start_time_formatted = f"{start_time // 60:02d}:{start_time % 60:02d},000"
-        end_time_formatted = f"{end_time // 60:02d}:{end_time % 60:02d},000"
+        start_time_formatted = f"{start_time // 3600000:02d}:{(start_time % 3600000) // 60000:02d}:{(start_time % 60000) // 1000:02d},{start_time % 1000:03d}"
+        end_time_formatted = f"{end_time // 3600000:02d}:{(end_time % 3600000) // 60000:02d}:{(end_time % 60000) // 1000:02d},{end_time % 1000:03d}"
 
         # Add to SRT output
         srt_output += f"{index + 1}\n{start_time_formatted} --> {end_time_formatted}\n{content}\n\n"
@@ -33,3 +33,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
